ff548fe049a9fb5a264b7448186d16a310a75218
Fernando Heleno <fernando.heleno@trtsp.jus.br>
2019-03-22 15:42:58 -0300

    Inicialização de repositório

---
+++ client/js/app/helpers/DataBind.js
@@ -0,0 +1,13 @@
+class DataBind {
+
+    constructor(model, view, ...props){
+        let proxy = ProxyFactory.create(model, props, (model) => {
+            view.update(model);
+        });
+
+        view.update(model);
+
+        return proxy;
+    }
+
+}
